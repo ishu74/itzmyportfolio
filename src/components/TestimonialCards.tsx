6d2cc54e05a481ef840af28de85643583f22dd56
Aayush <125236907+ishu74@users.noreply.github.com>
2024-04-13 01:18:05 +0530

    Your commit message here

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -31,12 +31,12 @@ const testimonials = [
       "title": "Full Stack Developer"
     },
     {
-      "quote": "It's rare to find a coder as versatile and dedicated as stive. They consistently go above and beyond to meet project requirements and exceed expectations.",
+      "quote": "It is rare to find a coder as versatile and dedicated as stive. They consistently go above and beyond to meet project requirements and exceed expectations.",
       "name": "Emily Wilson",
       "title": "Software Engineer"
     },
     {
-      "quote": "I've had the pleasure of mentoring stive during their coding journey, and I'm continually impressed by their rapid growth and eagerness to learn.",
+      "quote": "I have had the pleasure of mentoring stive during their coding journey, and I am continually impressed by their rapid growth and eagerness to learn.",
       "name": "Alex Rodriguez",
       "title": "Senior Developer"
     },
@@ -46,12 +46,12 @@ const testimonials = [
       "title": "Junior Developer"
     },
     {
-      "quote": "It's refreshing to work with someone as passionate and dedicated as stive. Their positive attitude and problem-solving skills make them a pleasure to collaborate with.",
+      "quote": "It is refreshing to work with someone as passionate and dedicated as stive. Their positive attitude and problem-solving skills make them a pleasure to collaborate with.",
       "name": "Daniel Evans",
       "title": "Software Architect"
     },
     {
-      "quote": "I've had the pleasure of witnessing stive's growth as a coder, and I have no doubt that they will continue to excel and make significant contributions to the field.",
+      "quote": "I have had the pleasure of witnessing stive growth as a coder, and I have no doubt that they will continue to excel and make significant contributions to the field.",
       "name": "Sophia Martinez",
       "title": "Software Developer"
     }
@@ -63,7 +63,7 @@ function TestimonialCards() {
         dark:bg-grid-white/[0.2] relative flex flex-col items-center
         justify-center overflow-hidden'>
             <h2 className='text-3xl font-bold text-center mb-8 z-10'>
-                Read Quote's: Voices of success</h2>
+                Read Quote&apos;s: Voices of success</h2>
             <div className='flex justify-center w-full overflow-hidded px-4 lg:px-8'>
                 <div className='w-full max-w-6xl'>
                     <InfiniteMovingCards
